fdcda471560808575b7c6e654710fd11a58008c0
sagar devre <sagar.devre@leucinetech.com>
2025-04-29 14:23:54 +0530

    Fixed the Request Handling and Response Handling

--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -2,7 +2,7 @@ const aiService = require('../services/ai.service');
 
 module.exports.getReview = async (req,res) => {
 
-    const prompt = req.query.prompt;
+    const prompt = req.body.prompt;
 
     if(!prompt) {
         return res.status(400).send('Promt is required');
